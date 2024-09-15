40e04b42161f214a94d7ce8a89ca8fc0223d7950
Jason <jason.yuchen.liu@gmail.com>
2024-09-15 02:25:57 -0400

    flask app basic structure

--- flask/app.py
+++ flask/app.py
@@ -1,2 +1,10 @@
 #run `pip install flask` to install flask
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/processAI', methods=['POST'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
